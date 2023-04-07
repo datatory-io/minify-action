13cf302708420ba18115ea25af0761476bb8796c
dtop <danilo.topalovic@nerdsee.com>
2023-04-07 23:13:28 +0200

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,11 @@
 DO_CSS=true
 DO_JS=true
 
+INPUT_CSS_DIR=${INPUT_CSS-DIR}
+INPUT_CSS_FILES=${INPUT_CSS-FILES}
+INPUT_OUTPUT_CSS=${INPUT_OUTPUT-CSS}
+
 declare -p
-env
 
 prepend_files_with_path() {
 
